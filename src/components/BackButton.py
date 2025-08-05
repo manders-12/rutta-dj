3a63c4a958a5cdd7d84552ddf09b99579abde86e
Michael Anderson <michaela121999@gmail.com>
2025-08-05 20:17:43 +0000

    fixed views and buttons

--- src/components/BackButton.py
+++ src/components/BackButton.py
@@ -1,12 +1,18 @@
 import discord
 from discord.ui import Button
 
-class BackButton():
+
+class BackButton(Button):
+
     def __init__(self, db, row, prev_view, prev_content):
-        super().__init__(label="Back", style=discord.ButtonStyle.danger, custom_id="back_ratings", row = row)
+        super().__init__(label="Back",
+                         style=discord.ButtonStyle.danger,
+                         custom_id="back_ratings",
+                         row=row)
         self.db = db
         self.prev_view = prev_view
         self.prev_content = prev_content
-    
+
     async def callback(self, interaction: discord.Interaction):
-        await interaction.response.edit_message(content = self.prev_content, view=self.prev_view)
+        await interaction.response.edit_message(content=self.prev_content,
+                                                view=self.prev_view)
