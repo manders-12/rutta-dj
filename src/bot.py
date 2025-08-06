f5a2b63fc39a1fbf905e6b81b3fdd06fb882460d
manders-12 <michaela121999@gmail.com>
2025-08-06 10:04:55 -0700

    refactor

--- src/bot.py
+++ src/bot.py
@@ -60,53 +60,6 @@ except Exception as e:
     raise
 
 
-# def extract_link(text):
-#     match = re.search(r'(https?://\S+)', text)
-#     return match.group(1) if match else None
-
-
-# def parse_rating(text):
-#     try:
-#         lines = text.split('\n')
-#         reviewI = None
-#         for i, line in enumerate(lines):
-#             parts = line.split('-')
-#             if len(parts) > 1 and parts[-1].strip().isdigit():
-#                 rating = parts[-1].strip()
-#                 reviewI = i + 1
-#                 break
-#         if reviewI is not None and reviewI < len(lines):
-#             explanation = '\n'.join(lines[reviewI:]).strip()
-#     except Exception as e:
-#         logging.error(f'Error parsing rating: {e}')
-#         rating, explanation = None, None
-#     return rating, explanation
-
-
-# def parse_recommendation(text):
-#     try:
-#         lines = text.split('\n')
-#         parts = lines[0].split('-')
-#         tag = parts[-1].strip()
-#         genre = "-".join(parts[0:-1]).strip()
-#     except Exception as e:
-#         logging.error(f'Error parsing recommendation: {e}')
-#         genre, tag = None, None
-#     return genre, tag
-
-
-# def parse_embed(embed):
-#     try:
-#         title = embed.title if embed.title else ''
-#         author = embed.author.name if embed.author else ''
-#         author = author.rstrip(' - Topic')
-#         link = embed.url if hasattr(embed, 'url') else None
-#     except Exception as e:
-#         logging.error(f'Error parsing embed: {e}')
-#         title, author, link = None, None, None
-#     return title, author, link
-
-
 def create_rating_embed(title, author, link, rating, explanation):
     try:
         embed = discord.Embed(title=f'Rating for {title}',
@@ -351,9 +304,8 @@ async def on_message_edit(before, after):
 
 
 try:
-    client.run(
-        os.getenv('DISCORD_TOKEN',
-                  'PUT YOUR TOKEN IN THE ENV FILE YOU DUMB IDIOT DUMMY'))
     logging.info('Bot Is Running.')
+    client.run(
+        os.getenv('DISCORD_TOKEN', 'PUT YOUR TOKEN IN THE ENV FILE YOU DUMB IDIOT DUMMY'))
 except discord.LoginFailure as e:
     logging.error(f'Bot Login Failure: {e}')
